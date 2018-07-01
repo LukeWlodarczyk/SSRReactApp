fa8f6822094bfa7adde3b4878893afe2ba264a7f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-02 01:18:05 +0200

    react hydrate

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
-import reacr from 'react';
+import React from 'react';
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<div>Home hello!!!!</div>
+			<button onClick={() => console.log('Hi!')}>Press</button>
+		</div>
+	);
 };
 
 export default Home;
