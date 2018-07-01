5240a8ce330013b48ea0b96e81df2da3e6d6ead5
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-01 23:59:29 +0200

    renderToString

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+	const content = renderToString(<Home />);
+
+	res.send(content);
+});
 
 const port = process.env.PORT || 3000;
 
