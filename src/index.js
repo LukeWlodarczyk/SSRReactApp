c74ea1e26a774482f434155bfb5ebdf10c912591
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-04 01:37:30 +0200

    let all promises complete before rendering content

--- src/index.js
+++ src/index.js
@@ -23,11 +23,17 @@ app.use(express.static('public'));
 app.get('*', async (req, res) => {
 	const store = createStore(req);
 
-	const promises = matchRoutes(Routes, req.path).map(
-		({ route: { loadData } }) => {
+	const promises = matchRoutes(Routes, req.path)
+		.map(({ route: { loadData } }) => {
 			return loadData ? loadData(store) : null;
-		}
-	);
+		})
+		.map(promise => {
+			if (promise) {
+				return new Promise((resolve, reject) => {
+					promise.then(resolve).catch(resolve);
+				});
+			}
+		});
 
 	await Promise.all(promises);
 
