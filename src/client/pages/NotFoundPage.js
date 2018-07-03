f5feb2c8c45b90568e6e93d4b0aa09341daec052
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-03 23:50:38 +0200

    NotFoundPage

---
+++ src/client/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div className="center-align" style={{ marginTop: '150px' }}>
+			<h3>Page not found</h3>
+		</div>
+	);
+};
+
+export default {
+	component: NotFound,
+};
