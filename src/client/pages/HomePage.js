9c01aab33ebd0bd8d85ec901fe5745f39a3f425e
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-04 21:45:44 +0200

    react-helmet

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
 	return (
 		<div className="center-align" style={{ marginTop: '150px' }}>
+			<Helmet>
+				<title>Home Page</title>
+				<meta property="og:title" content="Home" />
+			</Helmet>
 			<h3>Welcome</h3>
 			<p>Check out my ssr react app</p>
 		</div>
--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const NotFound = ({ staticContext = {} }) => {
 	staticContext.notFound = true;
 
 	return (
 		<div className="center-align" style={{ marginTop: '150px' }}>
+			<Helmet>
+				<title>Not found</title>
+				<meta property="og:title" content="Not found" />
+			</Helmet>
 			<h3>Page not found</h3>
 		</div>
 	);
